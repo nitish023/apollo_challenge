4a599f877fe5bb69e5064bc1426475c5118b2989
nitishchawla023 <158778719+nitishchawla023@users.noreply.github.com>
2024-12-05 08:03:21 -0500

    remove test.py file containing obsolete database connection

--- test.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-engine = create_engine('mysql+pymysql://root:Qwerty_105@localhost/apollo')
-try:
-    connection = engine.connect()
-    print("Database connected!")
-    connection.close()
-except Exception as e:
-    print("Database connection failed:", e)
